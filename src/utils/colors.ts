8403cf698e59f731224e0f126ef24f02f69c3beb
rhmvl <ramaabdiansyah03@gmail.com>
2025-10-12 11:05:48 +0700

    Update colors pallete.

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,7 +1,11 @@
 export const COLORS = {
-  background: 0x0,
-  empty: 0xFFFFFF,
-  start: 0x00ff00,
-  end: 0xff0000,
-  wall: 0x000000, 
+  background: 0x1e1e2e,
+  empty: 0xf5f5f5,
+  wall: 0x2e3440,
+  start: 0x81c784,
+  end: 0xe57373,
+  path: 0xab47bc,
+  openSet: 0x64b5f6,
+  closedSet: 0xba68c8,
+  frontier: 0xffa726,
 }
