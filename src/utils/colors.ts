f34d52ee79cbd5dc41df8c66d984462a9c5850e6
rhmvl <ramaabdiansyah03@gmail.com>
2025-10-18 15:26:59 +0700

    Update color code.

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -5,7 +5,19 @@ export const COLORS = {
   start: 0x81c784,
   end: 0xe57373,
   path: 0xab47bc,
-  openSet: 0x64b5f6,
-  closedSet: 0xba68c8,
-  frontier: 0xffa726,
+  closedSet: 0x64b5f6,
+  // closedSet: 0xba68c8,
+  openSet: 0xffa726,
 }
+
+export function hexToCss(hex: number) {
+  return "#" + hex.toString(16).padStart(6, "0");
+}
+
+export function rgbaToHex(rgba: string) {
+  const match = rgba.match(/rgba?\((\d+),\s*(\d+),\s*(\d+)/);
+  if (!match) return null;
+  const [_, r, g, b] = match.map(Number);
+  return (r << 16) + (g << 8) + b;
+}
+
